ba7f5b5ce0f65095492399fe2aaba3310a24b776
Louis Tsai <louis993546@gmail.com>
2015-11-03 00:18:08 +0800

    ADD: [inventory.js] connect to pgsql using client pool instead (much better performance)

--- routes/v1/inventory.js
+++ routes/v1/inventory.js
@@ -4,10 +4,12 @@
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
+
 var conString = "postgres://zepqdcvrvhsmgv:k4LI83mCEcXt3v1RFKv20AOjmr@ec2-54-83-29-15.compute-1.amazonaws.com:5432/d3n867p1e7dkp?ssl=true";
-//this initializes a connection pool
-//it will keep idle connections open for a (configurable) 30 seconds
-//and set a limit of 20 (also configurable)
+
+/**
+ * Client instance
+ */
 router.get('/dbdata', function (req, res) {
     var client = new pg.Client(conString);
     client.connect(function (err) {
@@ -28,6 +30,37 @@ router.get('/dbdata', function (req, res) {
             client.end();
         });
     });
+});
+
+/**
+ * Client pooling - Much faster
+ *
+ * this initializes a connection pool
+ * it will keep idle connections open for a (configurable) 30 seconds
+ * and set a limit of 20 (also configurable)
+ *
+ * Time when hit frequently:
+ * 1st: 2150.05 ms (Initiate during the first time, so much longer)
+ * 2nd: 251.18 ms
+ * 3rd: 250.77 ms
+ * 4th: 249.05 ms
+ * (1 minute break)
+ * 5th: 2029.853 ms
+ */
+
+router.get('/dbdata2', function (req, res) {
+    pg.connect(conString, function (err, client, done) {
+        if (err) {
+            return console.error('error fetching client from pool', err);
+        }
+        client.query('SELECT * FROM patients', function (err, result) {
+            done();  //call `done()` to release the client back to the pool
+            if (err) {
+                return console.error('error running query', err);
+            }
+            res.send(result.rows);
+        });
+    })
 });
 
 /* GET with item id */
