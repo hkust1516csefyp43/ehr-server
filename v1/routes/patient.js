529ddde396f56bd188ead5586fadb11c393e8e59
Louis Tsai <louis993546@gmail.com>
2015-11-20 12:17:47 +0800

    FIX: [patient.js] get list of patients partial implementation + utils fixes

--- v1/routes/patient.js
+++ v1/routes/patient.js
@@ -5,7 +5,8 @@ var express = require('express');
 var router = express.Router();
 var ba = require('basic-auth');
 var mysql = require('mysql');
-var su = require('../utils/string'); //FIXME I don't think this is the best practice
+var su = require('../utils'); //FIXME I don't think this is the best practice
+var sql = require('sql-bricks-postgres');
 
 /**
  * TODO
@@ -31,14 +32,87 @@ router.get('/:id', function(req, res) {
     res.send(message);
 });
 
-router.post('/', function (req, res) {
-    var body = req.body;
-    var message;
-    message = su.extend_or_replace(message, 'race: ' + body.race + '\n');
-    message = su.extend_or_replace(message, 'gender: ' + body.gender + '\n');
-    message = su.extend_or_replace(message, 'name: ' + body.name + '\n');
-    message = su.extend_or_replace(message, 'birthday: ' + body.birthday + '\n');
-    res.send(message);
+router.get('/', function (req, res) {
+    //var body = req.body;
+    //var message;
+    //message = su.extend_or_replace(message, 'race: ' + body.race + '\n');
+    //message = su.extend_or_replace(message, 'gender: ' + body.gender + '\n');
+    //message = su.extend_or_replace(message, 'name: ' + body.name + '\n');
+    //message = su.extend_or_replace(message, 'birthday: ' + body.birthday + '\n');
+    //res.send(message);
+
+    var params = {};
+    var param_query = req.query;
+    console.log(JSON.stringify(param_query));
+
+    var token = param_query.token;
+    if (!token) {
+        res.status(401).send('Token is missing');
+    } else {
+        params.token = token;
+    }
+
+    var next_station = param_query.next_station;
+    if (next_station) {
+        params.next_station = next_station;
+    }
+
+    //These 3 are mutually exclusive
+    var age = param_query.age;
+    var age_ot = param_query.age_ot;
+    var age_yt = param_query.age_yt;
+
+    var slum_id = param_query.slum_id;
+    if (slum_id) {
+        params.slum_id = slum_id;
+    }
+
+    var gender = param_query.gender;
+    if (gender) {
+        params.gender = gender;
+    }
+
+    var blood_type = param_query.blood_type;
+    if (blood_type) {
+        params.blood_type = blood_type;
+    }
+
+    var country_id = param_query.country_id;
+    if (country_id) {
+        params.country_id = country_id;
+    }
+
+    var email = param_query.email;
+    if (email) {
+        params.email = email;
+    }
+
+    var related_to_id = param_query.related_to_id;
+    if (related_to_id) {
+        params.related_id = related_to_id;
+    }
+
+    console.log(JSON.stringify(params));
+
+    var sql_query = sql
+        .select()
+        .from('patient')
+        .where(params)
+        .toString();
+
+    var limit = param_query.limit;
+    if (limit) {
+        sql_query.limit(limit);
+    }
+
+    var offset = param_query.offset;
+    if (offset) {
+        sql_query.offset(offset);
+    }
+
+    console.log(sql_query);
+
+    res.send('testing stuff');
 });
 
 //router.get('/test-mysql/', function (req, res, next) {
