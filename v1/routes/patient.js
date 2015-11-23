db9969b3a95bbf5fc4f69b7f17f5a84ddca3fdae
Louis Tsai <louis993546@gmail.com>
2015-11-24 00:13:23 +0800

    ADD: [patient.js] get list of patients (mostly) implemented

--- v1/routes/patient.js
+++ v1/routes/patient.js
@@ -7,6 +7,7 @@ var ba = require('basic-auth');
 var pg = require('pg');
 var util = require('../utils');
 var valid = require('../valid');
+var db = require('../database');
 var sql = require('sql-bricks-postgres');
 
 /* GET with patient id + basic auth */
@@ -41,105 +42,124 @@ router.get('/', function (req, res) {
         res.status(499).send('Token is missing');
         sent = true;
     } else {
-        params.token = token;
-    }
+        db.check_permission("reset_any_password", token, function (return_value, client) {
+            if (!return_value) {                                            //false (no token)
+                res.status(400).send('Token missing or invalid');
+            } else if (return_value.reset_any_password == false) {          //false (no permission)
+                res.status(403).send('No permission');
+            } else if (return_value.reset_any_password == true) {
+                var next_station = param_query.next_station;
+                if (next_station) {
+                    params.next_station = next_station;
+                }
 
-    var next_station = param_query.next_station;
-    if (next_station) {
-        params.next_station = next_station;
-    }
+                //These 3 are mutually exclusive
+                var age = param_query.age;
+                var age_ot = param_query.age_ot;
+                var age_yt = param_query.age_yt;
 
-    //These 3 are mutually exclusive
-    var age = param_query.age;
-    var age_ot = param_query.age_ot;
-    var age_yt = param_query.age_yt;
+                switch (util.mutually_exclusive(age, age_ot, age_yt)) {
+                    case 0:
+                        //Do nothing, LITERALLY
+                        break;
+                    case 1:
+                        if (age) {
+                            //TODO calculation
+                        } else if (age_ot) {
+                            //TODO calculation
+                        } else if (age_yt) {
+                            //TODO calculation
+                        }
+                        break;
+                    default:
+                        res.status(409).send('age, ago_ot and age_yt must be mutually exclusive');
+                        sent = true;
+                }
 
-    switch (util.mutually_exclusive(age, age_ot, age_yt)) {
-        case 0:
-            //Do nothing, LITERALLY
-            break;
-        case 1:
-            if (age) {
-                //TODO calculation
-            } else if (age_ot) {
-                //TODO calculation
-            } else if (age_yt) {
-                //TODO calculation
+                var slum_id = param_query.slum_id;
+                if (slum_id) {
+                    params.slum_id = slum_id;
+                }
+
+                var gender = param_query.gender;
+                if (gender) {
+                    params.gender = gender;
+                }
+
+                var blood_type = param_query.blood_type;
+                if (blood_type) {
+                    params.blood_type = blood_type;
+                }
+
+                var country_id = param_query.country_id;
+                if (country_id) {
+                    params.country_id = country_id;
+                }
+
+                var email = param_query.email;
+                if (email) {
+                    params.email = email;
+                }
+
+                //TODO get this from relationship table
+                //var related_to_id = param_query.related_to_id;
+                //if (related_to_id) {
+                //    params.related_id = related_to_id;
+                //}
+
+                //console.log(JSON.stringify(params));
+
+                var sql_query = sql
+                    .select()
+                    .from('patient')
+                    .where(params);
+
+                var limit = param_query.limit;
+                if (limit) {
+                    sql_query.limit(limit);
+                }
+
+                var offset = param_query.offset;
+                if (offset) {
+                    sql_query.offset(offset);
+                }
+
+                var sort_by = param_query.sort_by;
+                if (sort_by) {
+                    //TODO check if custom sort by param is valid
+                    sql_query.orderBy(sort_by);
+                } else {
+                    sql_query.orderBy('patient_id');
+                }
+
+                var limit = param_query.limit;
+                if (limit) {
+                    sql_query.limit(limit);
+                } else {    //Default limit
+                    sql_query.limit(100);
+                }
+
+                client.query(sql_query.toParams().text, sql_query.toParams().values, function (err, result) {
+                    if (err) {
+                        res.send('error fetching client from pool 2');
+                        sent = true;
+                        return console.error('error fetching client from pool', err);
+                    } else {
+                        res.json(result.rows);
+                    }
+                });
             }
-            break;
-        default:
-            res.status(409).send('age, ago_ot and age_yt must be mutually exclusive');
-            sent = true;
+        });
     }
 
-    var slum_id = param_query.slum_id;
-    if (slum_id) {
-        params.slum_id = slum_id;
-    }
-
-    var gender = param_query.gender;
-    if (gender) {
-        params.gender = gender;
-    }
-
-    var blood_type = param_query.blood_type;
-    if (blood_type) {
-        params.blood_type = blood_type;
-    }
-
-    var country_id = param_query.country_id;
-    if (country_id) {
-        params.country_id = country_id;
-    }
-
-    var email = param_query.email;
-    if (email) {
-        params.email = email;
-    }
-
-    var related_to_id = param_query.related_to_id;
-    if (related_to_id) {
-        params.related_id = related_to_id;
-    }
-
-    console.log(JSON.stringify(params));
-
-    var sql_query = sql
-        .select()
-        .from('patient')
-        .where(params);
-
-    var limit = param_query.limit;
-    if (limit) {
-        sql_query.limit(limit);
-    }
-
-    var offset = param_query.offset;
-    if (offset) {
-        sql_query.offset(offset);
-    }
-
-    var sort_by = param_query.sort_by;
-    if (sort_by) {
-        //TODO check if custom sort by param is valid
-        sql_query.orderBy(sort_by);
-    } else {
-        sql_query.orderBy('patient_id');
-    }
-
-    var limit = param_query.limit;
-    if (limit) {
-        sql_query.limit(limit);
-    } else {    //Default limit
-        sql_query.limit(100);
-    }
-
-    var sql_query_string = sql_query.toString();
-    console.log(sql_query_string);
-
-    if (!sent) {
-        res.send('testing stuff');
-    }
+    //
+    //
+    //var sql_query_string = sql_query.toString();
+    //console.log(sql_query_string);
+    //
+    //if (!sent) {
+    //    res.send('testing stuff');
+    //}
 });
 
 router.post('/', function (req, res) {
