5543417259d1dabc3fd9cd29a4ab06fb72e29d4f
RickyLo <wkloricky@gmail.com>
2015-11-27 10:55:44 +0800

    fetch all user info

--- v1/database.js
+++ v1/database.js
@@ -35,7 +35,7 @@ module.exports = {
                     sql_query = sql.select('r.' + permission);
                 }
                 sql_query = sql_query
-                    .select('t.user_id')
+                    .select('u.*')
                     .select('t.expiry_timestamp')
                     .from('users AS u, token AS t, role AS r')
                     .where(sql('t.token'), token)
