411e694df72cfb921cb61086cdf8936580beabc6
Louis Tsai <louis993546@gmail.com>
2015-11-29 03:28:31 +0800

    ADD: [database.js] caching result in 10x speed improvement

--- v1/routes/location.js
+++ v1/routes/location.js
@@ -4,12 +4,34 @@ var pg = require('pg');
 var util = require('../utils');
 var valid = require('../valid');
 var sql = require('sql-bricks-postgres');
+var NodeCache = require( "node-cache" );
+var locationCache = new NodeCache();
 
 /**
  * Get list of slums
  */
 router.get('/slum/', function (req, res) {
+  //TODO all other basic get data from db stuff
 
+  //TODO return cache if no extra input param (i.e. not searching)
+  locationCache.get( "allSlums", function( err, value ){
+    if( !err ){
+      if(value == undefined){
+        // key not found
+        //TODO create cache if cache does not exist yet
+        obj = { my: "Special", variable: 42 };
+        locationCache.set( "myKey", obj, function( err, success ){
+          if( !err && success ){
+            res.send(obj);
+          }
+        });
+
+      }else{
+        // value extracted in value
+        // TODO return it
+      }
+    }
+  });
 });
 
 /**
