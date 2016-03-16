b368e6b557fa37adee0e56cf587843ed05283d6d
Louis Tsai <louis993546@gmail.com>
2016-03-16 23:27:14 +0700

    ADD: add suitcase (end of suitcases.js)

--- v2/routes/suitcases.js
+++ v2/routes/suitcases.js
@@ -196,7 +196,62 @@ router.put('/:id', function (req, res) {
 });
 
 router.post('/', function (req, res) {
+  var sent = false;
+  var token = req.headers.token;
+  if (!token) {
+    res.status(errors.token_missing()).send('Token is missing');
+    sent = true;
+  } else {
+    db.check_token_and_permission("suitcases_read", token, function (err, return_value, client) {
+      if (!return_value) {
+        sent = true;
+        res.status(errors.bad_request()).send('Token missing or invalid');
+      } else if (return_value.suitcases_read === false) {
+        sent = true;
+        res.status(errors.no_permission).send('No permission');
+      } else if (return_value.suitcases_read === true) {
+        if (return_value.expiry_timestamp < Date.now()) {
+          sent = true;
+          res.status(errors.access_token_expired()).send('Access token expired');
+        } else {
+          var params = {};
+          params.suitcase_id = util.random_string(consts.id_random_string_length());
+          var name = req.body.name;
+          if (name)
+            params.suitcase_name = name;
+          else {
+            sent = true;
+            res.status(errors.bad_request()).send('name cannot be null');
+          }
 
+          var sql_query = sql.insert(consts.table_suitcases(), params).returning('*');
+
+          console.log("The whole query in string: " + sql_query.toString());
+
+          if (sent === false) {
+            client.query(sql_query.toParams().text, sql_query.toParams().values, function (err, result) {
+              if (err) {
+                res.send('error fetching client from pool');
+                sent = true;
+                return console.error('error fetching client from pool', err);
+              } else {
+                if (result.rows.length == 1) {
+                  q.save_sql_query(sql_query.toString());
+                  sent = true;
+                  res.json(result.rows[0]);
+                } else if (result.rows.length == 0) {
+                  res.status(errors.not_found()).send('Cannot find suitcase according to this id.');
+                } else {
+                  //how can 1 pk return more than 1 row!?
+                  res.status(errors.server_error()).send('Sth weird is happening');
+                }
+              }
+            });
+          }
+        }
+      }
+    });
+  }
 });
 
 router.delete('/:id', function (req, res) {
