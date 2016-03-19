5e4e4e9e569052389c2b35a6901cbe702291d97b
Louis Tsai <louis993546@gmail.com>
2016-03-19 16:51:37 +0700

    ADD: DELETE blocked device

--- v2/routes/blocked_devices.js
+++ v2/routes/blocked_devices.js
@@ -326,4 +326,54 @@ router.put('/:id', function (req, res) {
   }
 });
 
+router.delete('/:id', function (req, res) {
+  var sent = false;
+  var token = req.headers.token;
+  if (!token) {
+    res.status(errors.token_missing()).send('Token is missing');
+    sent = true;
+  } else {
+    db.check_token_and_permission("blocked_devices_write", token, function (err, return_value, client) {
+      if (!return_value) {
+        sent = true;
+        res.status(errors.bad_request()).send('Token missing or invalid');
+      } else if (return_value.blocked_devices_write === false) {
+        sent = true;
+        res.status(errors.no_permission).send('No permission');
+      } else if (return_value.blocked_devices_write === true) {
+        if (return_value.expiry_timestamp < Date.now()) {
+          sent = true;
+          res.status(errors.access_token_expired()).send('Access token expired');
+        } else {
+
+          var sql_query = sql.delete().from(consts.table_blocked_devices()).where(sql('blocked_device_id'), req.params.id).returning('*');
+          console.log("The whole query in string: " + sql_query.toString());
+
+          if (sent === false) {
+            client.query(sql_query.toParams().text, sql_query.toParams().values, function (err, result) {
+              if (err) {
+                res.status(errors.server_error()).send('error fetching client from pool: ' + err);
+                sent = true;
+                return console.error('error fetching client from pool', err);
+              } else {
+                if (result.rows.length === 1) {
+                  q.save_sql_query(sql_query.toString());
+                  sent = true;
+                  res.json(result.rows[0]);
+                } else if (result.rows.length === 0) {
+                  res.status(errors.not_found()).send('Cannot find blocked device according to this id.');
+                } else {
+                  //how can 1 pk return more than 1 row!?
+                  res.status(errors.server_error()).send('Sth weird is happening');
+                }
+              }
+            });
+          }
+        }
+      }
+    });
+  }
+
+});
+
 module.exports = router;
