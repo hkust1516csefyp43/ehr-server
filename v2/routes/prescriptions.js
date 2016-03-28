140a7bb7b9d818ef5809acdf7158815e0cc629dd
Louis Tsai <louis993546@gmail.com>
2016-03-28 11:15:23 +0700

    FIX: prescriptions.js

--- v2/routes/prescriptions.js
+++ v2/routes/prescriptions.js
@@ -47,16 +47,16 @@ router.get('/', function (req, res) {
           if (medication_id)
             params.medication_id = medication_id;
 
-          var prescription_detail = req.query.prescription_detail;
-          if (prescription_detail)
-            params.prescription_detail = prescription_detail;
-
           console.log(params);
 
           var sql_query = sql
             .select()
             .from(consts.table_prescriptions())
             .where(params);
+
+          var prescription_detail = req.query.prescription_detail;
+          if (prescription_detail)
+            sql_query.where(sql.ilike('prescription_detail', util.pre_suf_percent(prescription_detail)));
 
           var offset = param_query.offset;
           if (offset) {
@@ -163,7 +163,10 @@ router.post('/', function (req, res) {
   console.log(JSON.stringify(body));
   var token = param_headers.token;
   console.log(token);
-  if (!token) {
+  if (valid.empty_object(req.body)) {
+    sent = true;
+    res.status(errors.bad_request()).send('You cannot insert nothing');
+  } else if (!token) {
     res.status(errors.token_missing()).send('Token is missing');
     sent = true;
   } else {
@@ -205,51 +208,32 @@ router.post('/', function (req, res) {
           var sql_query = sql.insert(consts.table_prescriptions(), params).returning('*');
           console.log(sql_query.toString());
 
-          var offset = param_query.offset;
-          if (offset) {
-            sql_query.offset(offset);
-          }
-
-          var sort_by = param_query.sort_by;
-          if (sort_by) {
-            //TODO check if custom sort by param is valid
-            sql_query.orderBy(sort_by);
-          } else {
-            sql_query.orderBy('prescription_id');
-          }
-
-          var limit = param_query.limit;
-          if (limit) {
-            sql_query.limit(limit);
-          } else {    //Default limit
-            sql_query.limit(consts.list_limit());
-          }
-
-          client.query(sql_query.toParams().text, sql_query.toParams().values, function (err, result) {
-            if (err) {
-              res.status(errors.server_error()).send('error fetching client from pool: ' + err);
-              sent = true;
-              return console.error('error fetching client from pool', err);
-            } else {
-              if (result.rows.length === 1) {
-                q.save_sql_query(sql_query.toString());
+          if (!sent)
+            client.query(sql_query.toParams().text, sql_query.toParams().values, function (err, result) {
+              if (err) {
+                res.status(errors.server_error()).send('error fetching client from pool: ' + err);
                 sent = true;
-                res.json(result.rows[0]);
-              } else if (result.rows.length === 0) {
-                res.status(errors.not_found()).send('Insertion failed');
+                return console.error('error fetching client from pool', err);
               } else {
-                //how can 1 pk return more than 1 row!?
-                res.status(errors.server_error()).send('Sth weird is happening');
+                if (result.rows.length === 1) {
+                  q.save_sql_query(sql_query.toString());
+                  sent = true;
+                  res.json(result.rows[0]);
+                } else if (result.rows.length === 0) {
+                  res.status(errors.not_found()).send('Insertion failed');
+                } else {
+                  //how can 1 pk return more than 1 row!?
+                  res.status(errors.server_error()).send('Sth weird is happening');
+                }
               }
-            }
-          });
+            });
         }
       }
     });
   }
 });
 
-/*PUT*/
+/* PUT */
 router.put('/:id', function (req, res) {
   var sent = false;
   var params = {};
@@ -259,7 +243,10 @@ router.put('/:id', function (req, res) {
   console.log(JSON.stringify(body));
   var token = param_headers.token;
   console.log(token);
-  if (!token) {
+  if (valid.empty_object(req.body)) {
+    sent = true;
+    res.status(errors.bad_request()).send('You cannot edit nothing');
+  } else if (!token) {
     res.status(errors.token_missing()).send('Token is missing');
     sent = true;
   } else {
@@ -320,14 +307,6 @@ router.put('/:id', function (req, res) {
   }
 });
 
-/**
- * Delete prescription
- * TODO also actually delete the file
- * TODO better implementation:
- * just mark prescription as INACTIVE,and every time if someone try to access a
- * file that is inactive, return nothing and check if that file still exist. If
- * it does, remove it
- */
 router.delete('/:id', function (req, res) {
   var sent = false;
   var token = req.headers.token;
