aeb33c28135ac848aac233d928a6e0e50b9c0839
RickyLo <wkloricky@gmail.com>
2016-04-11 02:16:28 +0800

    UPDATE users.js change_info

--- v2/routes/users.js
+++ v2/routes/users.js
@@ -446,6 +446,139 @@ router.put('/:id', function (req, res) {
   }
 });
 
+/*PUT*/
+router.put('/change_info/:id', function (req, res) {
+  var sent = false;
+  var params = {};
+  var param_headers = req.headers;
+  var body = req.body;
+  console.log(JSON.stringify(param_headers));
+  console.log(JSON.stringify(body));
+  var token = param_headers.token;
+  console.log(token);
+  if (valid.empty_object(body)) {
+    sent = true;
+    res.status(errors.bad_request()).send('You cannot edit nothing');
+  } else if (!token) {
+    res.status(errors.token_missing()).send('Token is missing');
+    sent = true;
+  } else {
+    db.check_token_and_permission("users_read", token, function (err, return_value, client) {
+      if (!return_value) {                                        //return value == null >> sth wrong
+        res.status(errors.bad_request()).send('Token missing or invalid');
+      } else if (return_value.users_read === false) {          //false (no permission)
+        res.status(errors.no_permission()).send('No permission');
+      } else if (return_value.users_read === true) {           //w/ permission
+        if (return_value.user_id !== req.params.id) {
+          res.status(errors.no_permission()).send('No permission to edit other user');
+        } else if (return_value.expiry_timestamp < Date.now()) {
+          res.status(errors.access_token_expired()).send('Access token expired');
+        } else {
+
+          var gender_id = body.gender_id;
+          if (gender_id)
+            params.gender_id = gender_id;
+
+          var role_id = body.role_id;
+          if (role_id)
+            params.role_id = role_id;
+
+          var honorific = body.honorific;
+          if (honorific)
+            params.honorific = honorific;
+
+          var first_name = body.first_name;
+          if (first_name)
+            params.first_name = first_name;
+
+          var middle_name = body.middle_name;
+          if (middle_name)
+            params.middle_name = middle_name;
+
+          var last_name = body.last_name;
+          if (last_name)
+            params.last_name = last_name;
+
+          var nickname = body.nickname;
+          if (nickname)
+            params.nickname = nickname;
+
+          var username = body.username;
+          if (username)
+            params.username = username;
+
+          var email = body.email;
+          if (email)
+            params.email = email;
+
+          var salt = body.salt;
+          if (salt)
+            params.salt = salt;
+
+          var processed_password = body.processed_password;
+          if (processed_password)
+            params.processed_password = processed_password;
+
+          var birth_year = body.birth_year;
+          if (birth_year)
+            params.birth_year = birth_year;
+
+          var birth_month = body.birth_month;
+          if (birth_month)
+            params.birth_month = birth_month;
+
+          var birth_day = body.birth_day;
+          if (birth_day)
+            params.birth_day = birth_day;
+
+          var image_id = body.image_id;
+          if (image_id)
+            params.image_id = image_id;
+
+          var phone_country_code = body.phone_country_code;
+          if (phone_country_code)
+            params.phone_country_code = phone_country_code;
+
+          var phone_number = body.phone_number;
+          if (phone_number)
+            params.phone_number = phone_number;
+
+          if (valid.empty_object(params)) {
+            sent = true;
+            res.status(errors.bad_request()).send('You cannnot edit nothing');
+          }
+
+          var sql_query = sql
+            .update(consts.table_users(), params)
+            .where(sql('user_id'), req.params.id)
+            .returning('*');
+
+          console.log(sql_query.toString());
+          if (!sent)
+            client.query(sql_query.toParams().text, sql_query.toParams().values, function (err, result) {
+              if (err) {
+                res.status(errors.server_error()).send('error fetching client from pool: ' + err);
+                sent = true;
+                return console.error('error fetching client from pool', err);
+              } else {
+                if (result.rows.length === 1) {
+                  q.save_sql_query(sql_query.toString());
+                  sent = true;
+                  res.json(result.rows[0]);
+                } else if (result.rows.length === 0) {
+                  res.status(errors.not_found()).send('Cannot find user according to this id.');
+                } else {
+                  //how can 1 pk return more than 1 row!?
+                  res.status(errors.server_error()).send('Sth weird is happening');
+                }
+              }
+            });
+        }
+      }
+    })
+  }
+});
+
 router.delete('/:id', function (req, res) {
   var sent = false;
   var token = req.headers.token;
