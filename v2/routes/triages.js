77f33cd4fa54b33a29861cec67a1b809a2fe8fa5
RickyLo <wkloricky@gmail.com>
2016-04-12 15:21:36 +0800

    FIX debug triages.js

--- v2/routes/triages.js
+++ v2/routes/triages.js
@@ -87,10 +87,6 @@ router.get('/', function (req, res) {
           if (chief_complains)
             params.chief_complains = chief_complains;
 
-          var remark = req.query.remark;
-          if (remark)
-            sql_query.where(sql.ilike(consts.table_relationships(), util.pre_suf_percent(remark)));
-
           var start_timestamp = req.query.start_timestamp;
           if (start_timestamp)
             params.start_timestamp = start_timestamp;
@@ -111,6 +107,10 @@ router.get('/', function (req, res) {
             .select()
             .from(consts.table_triages())
             .where(params);
+
+          var remark = req.query.remark;
+          if (remark)
+            sql_query.where(sql.ilike(consts.table_relationships(), util.pre_suf_percent(remark)));
 
           var offset = param_query.offset;
           if (offset) {
