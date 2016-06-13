c410748088e8add9ca47ed339413b8202486520d
Louis Tsai <louis993546@gmail.com>
2016-06-13 11:11:19 +0700

    ADD: get and set clock API

--- v2/routes/static.js
+++ v2/routes/static.js
@@ -99,6 +99,60 @@ router.get('/status/', function (req, res) {
 });
 
 /**
+ * TODO test this API
+ */
+router.get('/clock/', function (req, res) {
+  var time = require('moment');
+  res.send(time);
+});
+
+/**
+ * TODO test this API
+ */
+router.put('/clock/', function (req, res) {
+  var token = req.headers.token;
+  var cloud = require('../../config.json').on_the_cloud;
+  if (cloud) {
+    res.status(errors.bad_request()).send('You cannot change time of the cloud server');
+  } else {
+    if (token) {
+      db.check_token_and_permission(null, token, function (err, return_value, client) {
+        if (err || !return_value) {
+          res.status(errors.server_error()).send('Something is wrong');
+        } else {
+          if (return_value.expiry_timestamp < Date.now()) {
+            res.status(errors.access_token_expired()).send('Access token expired');
+          } else {
+            var new_time = req.body.time;
+            if (!new_time)
+              res.status(errors.bad_request()).send('You have to tell me what time it is');
+            else {
+              var q1 = 'sudo date -s "' + new_time + '"';
+              shell.exec(q1, function(code, stdout, stderr){
+                console.log('Exit code 1:', code);
+                console.log('Program output 1:', stdout);
+                console.log('Program stderr 1:', stderr);
+                var q2 = 'sudo hwclock -w';
+                shell.exec(q2, function (code, stdout, stderr) {
+                  console.log('Exit code 2:', code);
+                  console.log('Program output 2:', stdout);
+                  console.log('Program stderr 2:', stderr);
+                  var response = {};
+                  response.time = require('moment');
+                  res.json(response);
+                });
+              });
+            }
+          }
+        }
+      });
+    } else {
+      res.status(errors.token_missing()).send('Token is missing');
+    }
+  }
+});
+
+/**
  * Receiving data and putting them into the db
  */
 router.put('/sync/', function (req, res) {
